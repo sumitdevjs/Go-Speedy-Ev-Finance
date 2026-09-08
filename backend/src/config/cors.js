964f4bf588f6867498629d3db1b3248929ca7a16
yana-malhotra27 <yana27.ym@gmail.com>
2026-09-08 20:46:54 +0530

    Fix CORS error blocking direct browser visits

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -3,8 +3,8 @@ const env = require('./env');
 
 const corsOptions = {
   origin: (origin, callback) => {
-    // If no FRONTEND_URL provided, or it's *, allow the requesting origin
-    if (!env.FRONTEND_URL || env.FRONTEND_URL === '*' || env.FRONTEND_URL === origin) {
+    // If no origin (e.g. direct browser visit), allow it
+    if (!origin || !env.FRONTEND_URL || env.FRONTEND_URL === '*' || env.FRONTEND_URL === origin) {
       callback(null, true);
     } else {
       callback(new Error('Not allowed by CORS'));
