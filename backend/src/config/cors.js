00da161f3878e1ed9ca9d6394d901082927e83b7
yana-malhotra27 <yana27.ym@gmail.com>
2026-09-08 17:51:38 +0530

    Fix Vercel 404 by routing to api/server and handling CORS correctly

--- backend/src/config/cors.js
+++ backend/src/config/cors.js
@@ -2,7 +2,14 @@ const cors = require('cors');
 const env = require('./env');
 
 const corsOptions = {
-  origin: env.FRONTEND_URL,
+  origin: (origin, callback) => {
+    // If no FRONTEND_URL provided, or it's *, allow the requesting origin
+    if (!env.FRONTEND_URL || env.FRONTEND_URL === '*' || env.FRONTEND_URL === origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true, // Allow cookies to be sent
   optionsSuccessStatus: 200,
 };
